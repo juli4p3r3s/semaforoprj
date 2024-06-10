ea90446512a49f08dff0500309721fb6642e8082
Aluno Dev24 <julia.peres0910@gmail.com>
2024-06-10 08:25:19 -0300

    WIFI - OK

--- semaforoprj.ino
+++ semaforoprj.ino
@@ -1,3 +1,25 @@
+//Biblioteca para utilizar wifi
+#include <WiFi.h>
+#include <WiFiClient.h>
+//Bibliotea para criar servidor http
+#include <WebServer.h>
+//Trabalhar com textos, padrão
+#include <string.h>
+
+//Cria instrancia da biblioteca  WebServer passando
+//a porta de funcionamento nesse caso a padrão 80
+WebServer server(80);
+
+//Definindo nome e senha da rede wifi
+const char *ssid = "wifi-IJHG";
+const char *password = "12345678";
+
+//Definindo a configuração de rede
+IPAddress local_IP(192,168,0,100);
+IPAddress gateway(19,168,0,1);
+IPAddress subnet(255,255,255,0);
+
+
 // Definição dos pinos dos LEDs
 const int ledVerde1 = 23;
 const int ledVerde2 = 19;
@@ -6,46 +28,52 @@ const int ledAmarelo2 = 18;
 const int ledVermelho1 = 21;
 const int ledVermelho2 = 5;
 
-void luzVe() {
+int tempoAmarelo= 2000;
+int tempoVerde= 5000;
+
+void primeiroEstagio() {
 digitalWrite(ledVerde1, HIGH);
-  digitalWrite(ledVerde2, LOW);
-  digitalWrite(ledAmarelo1, LOW);
-  digitalWrite(ledAmarelo2, LOW);
-  digitalWrite(ledVermelho1, LOW);
-  digitalWrite(ledVermelho2, HIGH);
-  delay(5000);
+digitalWrite(ledVermelho2, HIGH);
+delay(tempoVerde);
 }
 
-void luzA() {
+
+void segundoEstagio() {
   digitalWrite(ledVerde1, LOW);
-  digitalWrite(ledVerde2, LOW);
   digitalWrite(ledAmarelo1, HIGH);
-  digitalWrite(ledAmarelo2, LOW);
-  digitalWrite(ledVermelho1, LOW);
-  digitalWrite(ledVermelho2, HIGH);
-  delay(2000);
+  delay(tempoAmarelo);
 }
-void luzVyellow() {
-  digitalWrite(ledVerde1, LOW);
-  digitalWrite(ledVerde2, LOW);
-  digitalWrite(ledAmarelo1, HIGH);
-  digitalWrite(ledAmarelo2, LOW);
-  digitalWrite(ledVermelho1, LOW);
-  digitalWrite(ledVermelho2, HIGH);
-  delay(5000);
-}
-void luzVr() {
-  digitalWrite(ledVerde1, LOW);
+
+
+
+void terceiroEstagio() {
   digitalWrite(ledVerde2, HIGH);
   digitalWrite(ledAmarelo1, LOW);
-  digitalWrite(ledAmarelo2, LOW);
   digitalWrite(ledVermelho1, HIGH);
   digitalWrite(ledVermelho2, LOW);
-  delay(5000);
+  delay(tempoVerde);
 }
 
+void quartoEstagio(){
+  digitalWrite(ledVerde2, LOW);
+  digitalWrite(ledAmarelo2, HIGH);
+  delay(tempoAmarelo);
+}
+
+void quintoEstagio(){
+  digitalWrite(ledAmarelo2, LOW);
+  digitalWrite(ledVermelho1, LOW);
+}
 
 void setup() {
+   Serial.begin(115200);
+  //Configurando e iniciando o Acess Point com o ESP
+  Serial.print("Configurando Wifi...");
+  Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet)? "Okay" : "Erro na conf");
+  Serial.print("Iniciando a Wifi...");
+  Serial.println(WiFi.softAP(ssid, password)? "Okay" : "Erro na inicialização");
+  Serial.print("IP do AP: ");
+  Serial.println(WiFi.softAPIP());
   // Configuração dos pinos como saída
   pinMode(ledVerde1, OUTPUT);
   pinMode(ledVerde2, OUTPUT);
@@ -56,7 +84,9 @@ void setup() {
 }
 
 void loop() {
-  luzVe();
-  luzA();
-  luzVr();
+primeiroEstagio();
+segundoEstagio();
+terceiroEstagio();
+quartoEstagio();
+quintoEstagio();
 }
